369de23bb4327a495cb53a40c52e140f4a910afc
pixelmonaskarion <32988796+pixelmonaskarion@users.noreply.github.com>
2023-05-25 20:32:33 -0700

    Update attachment.rs

--- src/attachment.rs
+++ src/attachment.rs
@@ -17,7 +17,7 @@ pub async fn post_attachment<'a>(mut attachment: Form<Attachment<'a>>, server_ar
     // if !server_arc.lock().unwrap().tokens.lock().unwrap().contains_key(&attachment.token) {
     //     return Status::Forbidden
     // }
-    let path_string = format!("attachments\\{}.{}", attachment.uuid, attachment.extension);
+    let path_string = format!("attachments/{}.{}", attachment.uuid, attachment.extension);
     let path = Path::new(path_string.as_str());
     attachment.file.copy_to(path).await.unwrap();
     Status::Accepted
